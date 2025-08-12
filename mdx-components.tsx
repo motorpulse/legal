e9706cefe66235ba44de4b7342c9dffb335d9556
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-08-12 14:42:38 +0200

    ✍️ change header styles

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,17 @@
 import type { MDXComponents } from 'mdx/types';
 
+import { Heading } from '@/components/ui/kit';
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+function createHeadingComponent<Level extends 1 | 2 | 3 | 4 | 5>(level: Level) {
+  return {
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    [`h${level}`]: ({ children }) => (
+      <Heading level={level}>{children}</Heading>
+    ),
+  } satisfies Record<string, MDXComponents[`h${Level}`]>;
+}
+
 /**
  * Allows to make custom components for parsed MDX.
  * @param components
@@ -7,5 +19,10 @@ import type { MDXComponents } from 'mdx/types';
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    ...createHeadingComponent(1),
+    ...createHeadingComponent(2),
+    ...createHeadingComponent(3),
+    ...createHeadingComponent(4),
+    ...createHeadingComponent(5),
   };
 }
